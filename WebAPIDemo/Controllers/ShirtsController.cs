ea0fe3ae7b9d44c827c85e6f07ae73918f08cd9f
crishabhkumar <crishabhkumar@gmail.com>
2024-07-03 00:44:50 +0530

    Routing is done.

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -3,39 +3,34 @@
 namespace WebAPIDemo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
-    {   
+    {
         [HttpGet]
-        [Route("/shirts")]
         public string GetShirts()
         {
             return "Reading all the shirts.";
         }
 
         [HttpPost]
-        [Route("/shirts")]
         public string CreateShirt()
         {
             return "Creating a shirt.";
         }
 
-        [HttpGet]
-        [Route("/shirts/{id}")]
+        [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt by ID: {id}";
         }
 
-        [HttpPut]
-        [Route("/shirts/{id}")]
+        [HttpPut("{id}")]
         public string UpdateShirtById(int id)
         {
             return $"Updating shirt by ID: {id}";
         }
 
-        [HttpDelete]
-        [Route("/shirts/{id}")]
+        [HttpDelete("{id}")]
         public string DeleteShirtById(int id)
         {
             return $"Deleting shirt by ID: {id}";
