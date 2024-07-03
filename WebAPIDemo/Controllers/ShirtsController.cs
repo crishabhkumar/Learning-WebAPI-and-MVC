42b6a73ea507950b7d3616d6a54f2a46f0a6a1a5
crishabhkumar <crishabhkumar@gmail.com>
2024-07-03 13:38:59 +0530

    Return type of Action Method is done.

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -7,10 +7,25 @@ namespace WebAPIDemo.Controllers
     [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
-        [HttpGet]
-        public string GetShirts()
+        private List<Shirt> shirtList = new List<Shirt>()
         {
-            return "Reading all the shirts.";
+            new Shirt { ShirtId = 1, Brand = "Nike", Color = "Red", Size = 42, Gender = "Male", Price = 29.99 },
+            new Shirt { ShirtId = 2, Brand = "Adidas", Color = "Blue", Size = 40, Gender = "Female", Price = 25.99 },
+            new Shirt { ShirtId = 3, Brand = "Puma", Color = "Green", Size = 44, Gender = "Male", Price = 32.99 },
+            new Shirt { ShirtId = 4, Brand = "Reebok", Color = "Black", Size = 38, Gender = "Female", Price = 27.99 },
+            new Shirt { ShirtId = 5, Brand = "Under Armour", Color = "White", Size = 46, Gender = "Male", Price = 34.99 }
+        };
+
+
+        //[HttpGet]
+        //public string GetShirts()
+        //{
+        //    return "Reading all the shirts.";
+        //}
+        [HttpGet]
+        public IActionResult GetShirts()
+        {
+            return Ok("Reading all the shirts.");
         }
 
         //[HttpPost]
@@ -20,17 +35,42 @@ namespace WebAPIDemo.Controllers
         //}
 
         //FromBody Example
+        //[HttpPost]
+        //public string CreateShirt([FromBody] Shirt shirt)
+        //{
+        //    return "Creating a shirt.";
+        //}
+
         [HttpPost]
-        public string CreateShirt([FromBody] Shirt shirt)
+        public IActionResult CreateShirt([FromBody] Shirt shirt)
         {
-            return "Creating a shirt.";
+            return Ok("Creating a shirt.");
         }
 
+
         [HttpGet("{id}")]
-        public string GetShirtById(int id)
+        public IActionResult GetShirtById(int id)
         {
-            return $"Reading shirt by ID: {id}";
+            if(id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var shirt = shirtList.FirstOrDefault(x => x.ShirtId == id);
+
+            if (shirt == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(shirt);
         }
+
+        //[HttpGet("{id}")]
+        //public string GetShirtById(int id)
+        //{
+        //    return $"Reading shirt by ID: {id}";
+        //}
 
         #region FromRoute,FromQuery,FromHeader Examples
         ////FromRoute
@@ -55,16 +95,28 @@ namespace WebAPIDemo.Controllers
         //}
         #endregion
 
+        //[HttpPut("{id}")]
+        //public string UpdateShirtById(int id)
+        //{
+        //    return $"Updating shirt by ID: {id}";
+        //}
+
+        //[HttpDelete("{id}")]
+        //public string DeleteShirtById(int id)
+        //{
+        //    return $"Deleting shirt by ID: {id}";
+        //}
+
         [HttpPut("{id}")]
-        public string UpdateShirtById(int id)
+        public IActionResult UpdateShirtById(int id)
         {
-            return $"Updating shirt by ID: {id}";
+            return Ok($"Updating shirt by ID: {id}");
         }
 
         [HttpDelete("{id}")]
-        public string DeleteShirtById(int id)
+        public IActionResult DeleteShirtById(int id)
         {
-            return $"Deleting shirt by ID: {id}";
+            return Ok($"Deleting shirt by ID: {id}");
         }
 
     }
