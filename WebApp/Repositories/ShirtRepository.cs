133dd8f89e5a99f632e7cb5c73b5131e9d50530a
crishabhkumar <crishabhkumar@gmail.com>
2024-07-06 00:55:42 +0530

    Setting up the Shirts page with static data.

---
+++ WebApp/Repositories/ShirtRepository.cs
@@ -0,0 +1,73 @@
+﻿using WebApp.Models;
+
+namespace WebAPIDemo.Repositories
+{
+    public class ShirtRepository
+    {
+
+        private static List<Shirt> shirtList = new List<Shirt>()
+        {
+            new Shirt { ShirtId = 1, Brand = "Nike", Color = "Red", Size = 42, Gender = "Male", Price = 29.99 },
+            new Shirt { ShirtId = 2, Brand = "Adidas", Color = "Blue", Size = 40, Gender = "Female", Price = 25.99 },
+            new Shirt { ShirtId = 3, Brand = "Puma", Color = "Green", Size = 44, Gender = "Male", Price = 32.99 },
+            new Shirt { ShirtId = 4, Brand = "Reebok", Color = "Black", Size = 38, Gender = "Female", Price = 27.99 },
+            new Shirt { ShirtId = 5, Brand = "Under Armour", Color = "White", Size = 46, Gender = "Male", Price = 34.99 }
+        };
+
+        public static List<Shirt> GetShirts()
+        {
+            return shirtList;
+        }
+
+        public static void AddShirt(Shirt shirt)
+        {
+            shirt.ShirtId = shirtList.Max(x => x.ShirtId) + 1;
+            shirtList.Add(shirt);
+        }
+        public static bool ShirtExists(int id)
+        {
+            return shirtList.Exists(x => x.ShirtId == id);
+        }
+
+        public static Shirt? GetShirtById(int id)
+        {
+            return shirtList.FirstOrDefault(x => x.ShirtId == id);
+        }
+
+        public static Shirt? GetShirtByProperties(string? brand, string? gender, string? color, int? size)
+        {
+            return shirtList.FirstOrDefault(x => !string.IsNullOrWhiteSpace(brand)
+                                            && !string.IsNullOrWhiteSpace(x.Brand)
+                                            && x.Brand.Equals(brand, StringComparison.OrdinalIgnoreCase)
+                                            && !string.IsNullOrWhiteSpace(gender)
+                                            && !string.IsNullOrWhiteSpace(x.Gender)
+                                            && x.Gender.Equals(gender, StringComparison.OrdinalIgnoreCase)
+                                            && !string.IsNullOrWhiteSpace(color)
+                                            && !string.IsNullOrWhiteSpace(x.Color)
+                                            && x.Color.Equals(color, StringComparison.OrdinalIgnoreCase)
+                                            && size.HasValue
+                                            && x.Size.HasValue
+                                            && size.Value == x.Size.Value);
+        }
+
+        public static void UpdateShirt(Shirt shirt)
+        {
+            var shirtToUpdate = shirtList.First(x => x.ShirtId == shirt.ShirtId);
+            shirtToUpdate.Price = shirt.Price;
+            shirtToUpdate.Brand = shirt.Brand;
+            shirtToUpdate.Gender = shirt.Gender;
+            shirtToUpdate.Color = shirt.Color;
+            shirtToUpdate.Size = shirt.Size;
+        }
+
+        public static void DeleteShirt(int id)
+        {
+            Shirt shirt = shirtList.FirstOrDefault(x => x.ShirtId == id);
+            if (shirt != null)
+            {
+                shirtList.Remove(shirt);
+            }
+        }
+
+    }
+}
