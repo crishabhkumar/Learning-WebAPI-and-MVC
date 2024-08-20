dd8ae6c6cf87d2b9a55ecf65cba1911e69e5f29f
crishabhkumar <crishabhkumar@gmail.com>
2024-08-20 13:07:45 +0530

    Application entity addition.

---
+++ WebAPIDemo/Authority/AppRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPIDemo.Authority
+{
+    public static class AppRepository
+    {
+        private static List<Application> _applications = new List<Application>()
+        {
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MVCWebApp",
+                ClientId = "",
+                Secret = ""
+            }
+        };
+       
+    }
+}
